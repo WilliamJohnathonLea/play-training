50299764f21787533f788385fcf517bd3514cdd9
William Lea <wjlea93@gmail.com>
2019-08-13 18:11:21 +0100

    Add Dummy data to database

--- conf/evolutions/shop/1.sql
+++ conf/evolutions/shop/1.sql
@@ -1,4 +1,13 @@
 -- !Ups
 
+CREATE TABLE "orders" (
+    "id" INT PRIMARY KEY,
+    "drink" VARCHAR(30) NOT NULL,
+    "price" DOUBLE NOT NULL
+);
+
+INSERT INTO "orders" VALUES (1, 'Tea', 1.00), (2, 'Coffee', 1.20);
 
 -- !Downs
+
+DROP TABLE "orders";
